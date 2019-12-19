a2eae470520f2140c3251943dc7895203d6279a8
zhumingjie <zhumingjie@zhixuezhen.com>
2019-12-19 20:31:37 +0800

    优化分支差异性

--- mysite/views.py
+++ mysite/views.py
@@ -766,9 +766,10 @@ def ajax_createUatBranch(request):
                 else:
                     now = datetime.datetime.now()
                     branchCode = now.strftime('%Y%m%d%H%M')
-                    uid = '-'.join(str(uuid.uuid4()).split('-'))[0:5]
+                    uid = ''.join(str(uuid.uuid4()).split('-'))[0:5]
                     uatBranch = "uat-"
                     uatBranch += branchCode
+                    uatBranch += '-'
                     uatBranch += uid
                     branch_obj.create_branch(uatBranch)
                     status = branch_obj.merge_branch(project_plan_obj.devBranch, uatBranch)
